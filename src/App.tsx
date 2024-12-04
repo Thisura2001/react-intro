981d0ed5f60561159b324e1a9981df8920c7eaec
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-12-04 09:38:38 +0530

    day 2(onchange function)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-
 import './App.css'
-
+import {ChangeEvent, useState} from "react";
 function App() {
-  return (
-      <>
 
-      </>
-  )
+    const Customer = {
+        firstName: '',
+        lastName: ''
+    }
+
+    const [customer, setCustomer] = useState(Customer)
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        setCustomer({
+            ...customer,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return (
+        <>
+            <input name='firstName' type='text' placeholder='FirstName' onChange={handleChange}  />
+            <input name='lastName' type='text' placeholder='LastName' onChange={handleChange}  />
+            <br/>
+            <button>Submit</button><br/>
+            <h2>Hello..!  {customer.firstName} {customer.lastName}</h2>
+        </>
+    )
 }
+
+
+
 export default App
