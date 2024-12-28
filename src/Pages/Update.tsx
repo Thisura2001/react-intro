be5ffc295c54f2b50c89ddca1e1fd08fdb70663c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-12-28 14:26:35 +0530

    update item done with reducer

--- src/Pages/Update.tsx
+++ src/Pages/Update.tsx
@@ -2,6 +2,8 @@ import {Link, useNavigate} from "react-router";
 import {useContext, useState} from "react";
 import {CustomerContext} from "../Store/CustomerProvider.tsx";
 import {Customer} from "../Model/Customer.ts";
+import {itemContext} from "../Store/ItemProvider.tsx";
+import {Items} from "../Model/Items.ts";
 
 export function Update() {
     const navigate = useNavigate();
@@ -11,11 +13,23 @@ export function Update() {
     const [phone, setPhone] = useState('')
     const [address, setAddress] = useState('')
 
+    const [items,itemDispatch] = useContext(itemContext);
+    const [id, setId] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [price, setPrice] = useState("");
+    const [quantity, setQuantity] = useState("");
+
     function UpdateCustomers() {
         const updateCustomer = new Customer(name,email,phone,address)
         dispatch({type:'UPDATE_CUSTOMER',payload:updateCustomer});
         navigate("/");
 
+    }
+
+    function updateItems() {
+        const newItem =  new Items(id,itemName,price,quantity)
+        itemDispatch({type:'UPDATE_ITEM',payload:newItem});
+        navigate("/");
     }
 
     return(
@@ -64,11 +78,30 @@ export function Update() {
                         <h1 className="font-extrabold text-4xl text-cyan-500">Update Items</h1>
                     </header>
                     <div className="mt-6">
-                        <input type="text" placeholder="Enter the item Name" className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"/>
-                        <input type="text" placeholder="Enter the item price" className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"/>
-                        <input type="text" placeholder="Enter the item Quntity" className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"/>
-
-                        <button  className="mt-6 w-full bg-amber-400 text-white font-bold py-2 px-4 rounded-lg hover:bg-amber-500 transition duration-200">Update Items</button>
+                        <input type="text"
+                               placeholder="Enter the item Id"
+                               className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"
+                               onChange={(e) => setId(e.target.value)}
+                        />
+                        <input type="text"
+                               placeholder="Enter the item Name"
+                               className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"
+                               onChange={(e) => setItemName(e.target.value)}
+                        />
+                        <input type="text"
+                               placeholder="Enter the item price"
+                               className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"
+                               onChange={(e) => setPrice(e.target.value)}
+                        />
+                        <input type="text"
+                               placeholder="Enter the item Quntity"
+                               className="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 m-4"
+                               onChange={(e) => setQuantity(e.target.value)}
+                        />
+                        <button
+                            className="mt-6 w-full bg-amber-400 text-white font-bold py-2 px-4 rounded-lg hover:bg-amber-500 transition duration-200" onClick={updateItems}>Update
+                            Items
+                        </button>
                     </div>
                 </div>
             </div>
